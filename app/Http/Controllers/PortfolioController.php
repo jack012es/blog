ad3dae269fa6d70df103d8e66d7431b38901e179
Jack <jacksonmixproject@gmail.com>
2021-01-15 14:00:03 -0500

    active en rutas y href

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -13,72 +13,12 @@ class PortfolioController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $portfolio = [
+            ['title' => 'Proyecto 1'],
+            ['title' => 'Proyecto 2'],
+            ['title' => 'Proyecto 3'],
+            ['title' => 'Proyecto 4']
+        ];
+        return view('portfolio', compact('portfolio'));
     }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>@yield('title', 'Prueba')</title>
+    <style>
+        .active a {
+            color: red;
+            text-decoration: none;
+        }
+
+    </style>
 </head>
+
 <body>
     <nav>
+
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li class="{{ request()->routeIs('home') ? 'active' : '' }}"><a href="/">Home</a></li>
+            <li class="{{ request()->routeIs('about') ? 'active' : '' }}"><a href="/about">About</a></li>
+            <li class="{{ request()->routeIs('portfolio') ? 'active' : '' }}"><a href="/portfolio">Portfolio</a></li>
+            <li class="{{ request()->routeIs('contact') ? 'active' : '' }}"><a href="/contact">Contact</a></li>
         </ul>
     </nav>
     @yield('content')
 </body>
+
 </html>
