ebfb888c05cc6fb2079d518421f0fa74fdc86615
Jack <jacksonmixproject@gmail.com>
2021-01-15 14:14:50 -0500

    corregida y separada la navegacion se agrego el archivo helpers para el autoload de laravel

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,15 +13,7 @@
 </head>
 
 <body>
-    <nav>
-
-        <ul>
-            <li class="{{ request()->routeIs('home') ? 'active' : '' }}"><a href="/">Home</a></li>
-            <li class="{{ request()->routeIs('about') ? 'active' : '' }}"><a href="/about">About</a></li>
-            <li class="{{ request()->routeIs('portfolio') ? 'active' : '' }}"><a href="/portfolio">Portfolio</a></li>
-            <li class="{{ request()->routeIs('contact') ? 'active' : '' }}"><a href="/contact">Contact</a></li>
-        </ul>
-    </nav>
+    @include('partials.nav')
     @yield('content')
 </body>
 
