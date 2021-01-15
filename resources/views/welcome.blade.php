862745b476233545fee4c99c1b1f28688ac47411
Jack <jacksonmixproject@gmail.com>
2021-01-14 23:11:44 -0500

    Prueba de ruta 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+@extends("layouts.app")
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -6,7 +7,7 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
+        <!-- Fontsa -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
