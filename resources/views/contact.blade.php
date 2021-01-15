ed056f870b63c816c72505412397f059b135999b
Jack <jacksonmixproject@gmail.com>
2021-01-15 14:46:09 -0500

    .

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,5 +4,12 @@
 
 @section('content')
     <h1>Contact</h1>
-
+    <form action="{{ route('contact') }}" method="POST">
+        @csrf
+        <input type="text" name="name" placeholder="Nombre.."><br>
+        <input type="text" name="email" placeholder="Email.."><br>
+        <input type="text" name="subject" placeholder="Asunto.."><br>
+        <textarea name="content" placeholder="Mensaje..." cols="30" rows="10"></textarea><br>
+        <button>Enviar</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,6 @@ Route::view('/about', 'about')->name('about');
 Route::get('/portfolio', 'PortfolioController@index')->name('portfolio');
 
 Route::view('/contact', 'contact')->name('contact');
+
+
+Route::post('contact', 'MessagesController@store');
